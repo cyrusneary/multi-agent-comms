fb355117f95529a1dbd6e77fb1bb28db212d2798
Cyrus Neary <cyrusneary@pop-os.localdomain>
2021-09-15 13:29:21 -0500

    Finished simple gridworld MDP example.

--- environments/gridworld.py
+++ environments/gridworld.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 import sys
 sys.path.append('..')
@@ -10,9 +11,10 @@ class Gridworld(object):
                 Nr : int = 5, 
                 Nc : int = 5,
                 slip_p : float = 0.05,
-                initial_state : int = 0,
-                target_states : list = [24],
-                dead_states : list = []
+                initial_state : tuple = (0,0),
+                target_states : list = [(4,4)],
+                dead_states : list = [],
+                walls : list = []
                 ):
         """
         Initializer for the Gridworld object.
@@ -26,11 +28,13 @@ class Gridworld(object):
         slip_p :
             Slip probability. Should be 0 <= slip_p <= 1
         initial_state :
-            Index of the initial state of the gridworld.
+            Position of the initial state of the gridworld.
         target_states :
-            List of indexes of target states in the gridworld.
+            List of positions of target states in the gridworld.
         dead_states :
-            List of indexes of dead states in the gridworld.
+            List of positions of dead states in the gridworld.
+        walls :
+            List of positions of the walls in the gridworld.
         """
 
         self.Nr = Nr
@@ -39,16 +43,25 @@ class Gridworld(object):
 
         self._construct_state_space()
 
-        assert initial_state <= self.Ns - 1
         self.initial_state = initial_state
+        self.initial_index = self.index_from_pos[initial_state]
+        assert self.initial_index <= self.Ns - 1
 
         assert len(target_states) > 0
         self.target_states = target_states
+        self.target_indexes = [self.index_from_pos[t_state] 
+                                for t_state in self.target_states]
 
         assert slip_p <= 1.0
         self.slip_p = slip_p
 
         self.dead_states = dead_states
+        self.dead_indexes = [self.index_from_pos[d_state]
+                                for d_state in self.dead_states]
+
+        self.walls = walls
+        self.wall_indexes = [self.index_from_pos[w_state]
+                                for w_state in self.walls]
 
         # Actions:
         # 0 : right
@@ -58,7 +71,6 @@ class Gridworld(object):
         self.Na = 4
 
         self._build_transition_matrix()
-        self._build_mdp()
 
     def _construct_state_space(self):
         """
@@ -75,11 +87,16 @@ class Gridworld(object):
     def _build_transition_matrix(self):
         """
         Build the transition matrix storing the dynamics of the 
-        gridworld environment. Given an action, the environment 
-        proceeds to the desired next state with prob 1.0 - slip_p.
-        The remaining slip_p is distributed among the remaining adjacent
-        states. If the selected action moves into a wall, then all of 
-        the probability is assigned to the available adjacent states.
+        gridworld environment. 
+        
+        self.T[s,a,s'] = probability of reaching state s' from state s
+                            under action a.
+        
+        Given an action, the environment proceeds to the desired next 
+        state with prob 1.0 - slip_p. The remaining slip_p is 
+        distributed among the remaining adjacent states. If the selected
+        action moves into a wall, then all of the probability is 
+        assigned to the available adjacent states.
         """
         self.T = np.zeros((self.Ns, self.Na, self.Ns))
 
@@ -87,7 +104,7 @@ class Gridworld(object):
 
             # Check if the state is absorbing before assigning 
             # any probability values.
-            if (s in self.target_states) or (s in self.dead_states):
+            if (s in self.target_indexes) or (s in self.dead_states):
                 continue
 
             state_r, state_c = self.pos_from_index[s]
@@ -136,7 +153,7 @@ class Gridworld(object):
                         self.T[s, a, s_next] = 1.0 / num_slips
 
         # Set all target states to be absorbing
-        for state in self.target_states:
+        for state in self.target_indexes:
             for action in range(self.Na):
                 self.T[state, action, state] = 1.0
 
@@ -145,20 +162,150 @@ class Gridworld(object):
             for action in range(self.Na):
                 self.T[state, action, state] = 1.0
 
-    def _build_mdp(self):
+    def build_mdp(self, gamma : float = 1.0):
         """Build an MDP model of the environment."""
-        self.mdp = MDP(self.Nr * self.Nc, 
-                        self.Na, 
-                        self.T, 
-                        self.initial_state,
-                        self.target_states,
-                        self.dead_states,
-                        )
+        return MDP(self.Nr * self.Nc, 
+                    self.Na, 
+                    self.T, 
+                    self.initial_index,
+                    self.target_indexes,
+                    self.dead_states,
+                    gamma=gamma
+                    )
+
+    def display(self, state=None, ax=None, plot=False, highlighted_states=None):
+
+        # Display parameters
+        grid_spacing = 1
+        max_x = grid_spacing * self.Nc
+        max_y = grid_spacing * self.Nr
+
+        if ax is None:
+            fig = plt.figure(figsize=(8,8))
+            ax = fig.add_subplot(111, aspect='equal')
+            plot = True
+
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+
+        # Plot gridworld lines
+        for i in range(self.Nr + 1):
+            ax.plot([0, max_x], [- i * grid_spacing, - i * grid_spacing], color='black')
+        for j in range(self.Nc + 1):
+            ax.plot([j * grid_spacing, j * grid_spacing], [0, -max_y], color='black')
+
+        # Plot the initial state
+        (init_r, init_c) = self.initial_state
+        ax.plot(init_c * grid_spacing + grid_spacing/2, 
+                - (init_r * grid_spacing + grid_spacing/2), 
+                linestyle=None, marker='x', markersize=15, color='blue')
+
+        # plot the current state
+        if state is not None:
+            (state_r, state_c) = state
+            ax.plot(state_c * grid_spacing + grid_spacing/2,
+                    -(state_r * grid_spacing + grid_spacing/2), 
+                    linestyle=None, marker='o', markersize=20, color='blue')
+
+        # Plot the target locations
+        for goal in self.target_states:
+            (goal_r, goal_c) = goal
+            goal_square = patches.Rectangle((goal_c, 
+                                            -(goal_r + 1) * grid_spacing), 
+                                            grid_spacing, grid_spacing, 
+                                            fill=True, color='green')
+            ax.add_patch(goal_square)
+
+        # Plot walls
+        for wall in self.walls:
+            (wall_r, wall_c) = wall
+            wall_square = patches.Rectangle((wall_c * grid_spacing, 
+                                            -(wall_r + 1) * grid_spacing), 
+                                            grid_spacing, grid_spacing, 
+                                            fill=True, color='black')
+            ax.add_patch(wall_square)
+
+        # Plot the obstacles
+        for obstacle in self.dead_states:
+            (obs_r, obs_c) = obstacle
+            obs_square = patches.Rectangle((obs_c * grid_spacing, 
+                                            -(obs_r + 1) * grid_spacing), 
+                                            grid_spacing, grid_spacing, 
+                                            fill=True, color='red')
+            ax.add_patch(obs_square)
+
+        if plot:
+            plt.show()
+
+    def visualize_occupancy_measures(self, 
+                                    occupancy_vals, 
+                                    ax=None, 
+                                    plot=True, 
+                                    color='black', 
+                                    highlighted_states=None):
+
+        # Display parameters
+        grid_spacing = 1
+        max_x = grid_spacing * self.Nc
+        max_y = grid_spacing * self.Nr
+
+        if ax is None:
+            fig = plt.figure(figsize=(8,8))
+            ax = fig.add_subplot(111, aspect='equal')
+            plot = True
+
+        self.display(ax=ax, highlighted_states=highlighted_states)
+
+        max_occupancy_val = np.max(occupancy_vals)
+
+        for s in range(self.Ns):
+            if not(s in self.wall_indexes) \
+                and not(s in self.dead_indexes) \
+                    and not(s in self.target_indexes):
+
+                row, col = self.pos_from_index[s]
+
+                for action in range(self.Na):
+                    x = occupancy_vals[s, action]
+                    if action == 0: # trying to move right
+                        next_row = row
+                        next_col = col + 1
+
+                    if action == 1: # trying to move up
+                        next_row = row - 1
+                        next_col = col
+
+                    if action == 2: # trying to move left
+                        next_row = row
+                        next_col = col - 1
+
+                    if action == 3: # trying to move down
+                        next_row = row + 1
+                        next_col = col
+
+                    start_x = col * grid_spacing + grid_spacing/2
+                    start_y = -(row * grid_spacing + grid_spacing/2)
+
+                    dx = (next_col - col) * grid_spacing
+                    dy = -(next_row - row) * grid_spacing
+
+                    ax.arrow(start_x, start_y, dx, dy,
+                            shape='full', linewidth=2.0, 
+                            length_includes_head=True,
+                            head_width=0.2, color=color, 
+                            alpha=np.log(x/max_occupancy_val+1.0))
+
+        if plot:
+            plt.show()
 
 def main():
     gridworld = Gridworld(Nr=5, Nc=5)
-    gridworld.mdp.build_model()
-    print(gridworld)
+    mdp = gridworld.build_mdp(gamma=0.99)
+    prob, x = mdp.build_reachability_LP()
+    prob.solve()
+    policy = mdp.policy_from_occupancy_vars(x.value)
+    print(policy)
+    gridworld.visualize_occupancy_measures(x.value)
 
 if __name__ == '__main__':
     main()
--- environments/mdp.py
+++ environments/mdp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cvxpy as cp
+from numpy.lib.function_base import average
 
 class MDP(object):
     """Class representing an MDP"""
@@ -11,6 +12,7 @@ class MDP(object):
                 initial_state: int,
                 target_set: list,
                 dead_states: list,
+                gamma: float = 1.0,
                 ):
         """
         
@@ -30,6 +32,8 @@ class MDP(object):
             Array of state indexes representing the target states.
         dead_states :
             Array of state indexes representing the absorbing states.
+        gamma :
+            Discount factor of the MDP.
         """
         assert num_states > 0 and num_actions > 0
         self.Ns = num_states
@@ -47,17 +51,92 @@ class MDP(object):
         if len(dead_states) > 0: assert np.max(dead_states) <= self.Ns - 1
         self.dead_states = dead_states
 
-    def build_opt_problem(self):
+        assert 0.0 <= gamma and gamma <= 1.0
+        self.gamma = gamma
 
-        # Define the problem variables
+        # Get indexes of all absorbing and "active" states
+        absorbing_states = set(self.target_set).union(set(self.dead_states))
+        active_states = set(range(self.Ns)).difference(absorbing_states)
+        self.absorbing_states = list(absorbing_states)
+        self.active_states = list(active_states)
+
+    def build_reachability_LP(self):
+        """
+        Construct an LP to solve for a set of occupancy measures that
+        respect the MDP dynamics while maximizing the probability of
+        reaching the target set.
+
+        Returns
+        -------
+        prob : cvxpy Problem
+            The linear program to be solved.
+        x : cvxpy Variable
+            A Ns x Na variable where x[s,a] represents the occupancy
+            measure of state-action pair (s,a).
+        """
+
+        ##### Define the problem variables
         x = cp.Variable(shape=(self.Ns, self.Na), name='x')
+        gamma = cp.Parameter(name='gamma', value=self.gamma)
 
-        # Define the problem constraints
+        ##### Define the problem constraints
         constraints = []
-        constraints.append(x >= 0)
-        constraints.append(x[self.target_set] == 0)
-        if len(self.dead_states > 0):
-            constraints.append(x[self.dead_states] == 0)
 
-    def policy_from_occupancy_vars(self, occupancy_vars):
-        pass
+        # All occupancy measures are positive
+        constraints.append(x[self.active_states, :] >= 0)
+
+        # Absorbing states are set to have no occupancy measure
+        constraints.append(x[self.absorbing_states, :] == 0)
+
+        # The remaining states satisfy the balance equations
+        occupancy_out = cp.sum(x[self.active_states, :], axis=1)
+
+        occupancy_init = np.zeros((self.Ns,))
+        occupancy_init[self.initial_state] = 1.0
+        occupancy_init = occupancy_init[self.active_states]
+
+        occupancy_in = cp.hstack(
+            [gamma * cp.sum(cp.multiply(x[self.active_states, :], 
+                                self.T[self.active_states, :, i])) 
+                for i in self.active_states]
+            )
+
+        constraints.append(occupancy_out - occupancy_init - occupancy_in == 0)
+
+        ##### Define the problem objective
+        target_set_in = cp.sum(cp.hstack(
+                [cp.sum(cp.multiply(x[self.active_states, :], 
+                                    self.T[self.active_states, :, i])) 
+                    for i in self.target_set]
+                )
+            )
+
+        obj = cp.Maximize(target_set_in)
+
+        ##### Create the problem
+        prob = cp.Problem(obj, constraints)
+
+        return prob, x
+
+    def policy_from_occupancy_vars(self, x : np.ndarray):
+        """
+        Build a policy from the occupancy measure values.
+
+        Parameters
+        ----------
+        x :
+            Array built such that x[s,a] represents the occupancy 
+            measure of state-action pair (s,a).
+
+        Returns
+        -------
+        policy : ndarray
+            Array built such that policy[s,a] represents the probability
+            of taking action a from state s under this policy.
+            Note that policy[s,a] = 0 for all a if s is absorbing.
+        """
+        policy = np.zeros((self.Ns, self.Na))
+        for s in self.active_states:
+            for a in range(self.Na):
+                policy[s,a] = x[s, a] / np.sum(x[s, :])
+        return policy
