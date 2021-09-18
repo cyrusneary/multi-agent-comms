33241919480de1777c66cb97da4303cb317d4cd3
Cyrus Neary <cyrusneary@gmail.com>
2021-09-18 15:27:13 -0500

    Cleaning up code.

--- examples/ma_gridworld_joint_reachability.py
+++ examples/ma_gridworld_joint_reachability.py
@@ -9,40 +9,40 @@ from markov_decision_process.mdp import MDP
 from optimization_problems.reachability_LP import build_reachability_LP,\
                                                     process_occupancy_vars
 
-# #### BUILD THE GRIDWOLRD FROM SCRATCH
+#### BUILD THE GRIDWOLRD FROM SCRATCH
 
-# # Build the gridworld
-# print('Building gridworld')
-# t_start = time.time()
-# gridworld = MAGridworld(N_agents=2,
-#                         Nr=5, 
-#                         Nc=5,
-#                         slip_p=0.1,
-#                         initial_state=(4, 0, 4, 4),
-#                         target_states=[(4, 4, 4, 0)],
-#                         dead_states=[],
-#                         lava=[(0, 4), (0, 0)],
-#                         walls=[(0,2), (2,2), (4,2)])
+# Build the gridworld
+print('Building gridworld')
+t_start = time.time()
+gridworld = MAGridworld(N_agents=2,
+                        Nr=5, 
+                        Nc=5,
+                        slip_p=0.1,
+                        initial_state=(4, 0, 4, 4),
+                        target_states=[(4, 4, 4, 0)],
+                        dead_states=[],
+                        lava=[(0, 4), (0, 0)],
+                        walls=[(0,2), (2,2), (4,2)])
 
-# print('Constructed gridworld in {} seconds'.format(time.time() - t_start))
+print('Constructed gridworld in {} seconds'.format(time.time() - t_start))
 
-# # Sanity check on the transition matrix
-# for s in range(gridworld.Ns_joint):
-#     for a in range(gridworld.Na_joint):
-#         assert(np.abs(np.sum(gridworld.T[s, a, :]) - 1.0) <= 1e-12)
+# Sanity check on the transition matrix
+for s in range(gridworld.Ns_joint):
+    for a in range(gridworld.Na_joint):
+        assert(np.abs(np.sum(gridworld.T[s, a, :]) - 1.0) <= 1e-12)
 
-# # Save the constructed gridworld
-# save_file_str = os.path.join(os.path.abspath(os.path.curdir),
-#                                 '..', 'environments',
-#                                 'saved_environments', 'ma_gridworld.pkl')
-# gridworld.save(save_file_str)
-
-##### LOAD A PRE-CONSTRUCTED GRIDWORLD
-load_file_str = os.path.join(os.path.abspath(os.path.curdir),
+# Save the constructed gridworld
+save_file_str = os.path.join(os.path.abspath(os.path.curdir),
                                 '..', 'environments',
                                 'saved_environments', 'ma_gridworld.pkl')
-gridworld = MAGridworld(load_file_str=load_file_str)
-print('Loaded multiagent gridworld.')
+gridworld.save(save_file_str)
+
+# ##### LOAD A PRE-CONSTRUCTED GRIDWORLD
+# load_file_str = os.path.join(os.path.abspath(os.path.curdir),
+#                                 '..', 'environments',
+#                                 'saved_environments', 'ma_gridworld.pkl')
+# gridworld = MAGridworld(load_file_str=load_file_str)
+# print('Loaded multiagent gridworld.')
 
 ##### Examine the tradeoff between success probability and expected len
 mdp = gridworld.build_mdp()
@@ -61,7 +61,7 @@ for exp_len_coeff_val in exp_len_coeff_list:
     succ_prob_list.append(mdp.success_probability_from_occupancy_vars(occupancy_vars))
     exp_len_list.append(mdp.expected_len_from_occupancy_vars(occupancy_vars))
 
-fig = plt.figure(figsize=(20,10))
+fig = plt.figure()
 ax = fig.add_subplot(211)
 ax.plot(exp_len_coeff_list, succ_prob_list, 
         linewidth=3, marker='o', label='Success Probability')
@@ -109,7 +109,7 @@ print('Imaginary play success rate: {}'.format(success_count / num_trajectories)
 num_trajectories = 10
 trajectory_list = []
 for t_ind in range(num_trajectories):
-    trajectory_list.append(gridworld.run_trajectory(policy, max_steps=50))
+    trajectory_list.append(gridworld.run_trajectory_imaginary(policy, max_steps=50))
 
 gif_save_folder = os.path.join(os.path.abspath(os.path.curdir), 'gifs')
 
