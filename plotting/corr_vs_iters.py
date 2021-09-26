982c02ccebb8a7f71d8a889a29e49deaa405d395
Cyrus Neary <cyrusneary@gmail.com>
2021-09-25 20:20:14 -0500

    Changed dataset being used to generate plots.

--- plotting/corr_vs_iters.py
+++ plotting/corr_vs_iters.py
@@ -15,12 +15,12 @@ fontsize = 12
 tikz_save_path = os.path.abspath(os.path.join(os.path.curdir, 'tikz'))
 
 base_path = os.path.abspath(os.path.join(os.path.curdir, '..', 'examples', 'results'))
-save_file_name = '2021-09-20-17-10-46_ma_gridworld_total_corr_slip_0p05.pkl'
+save_file_name = '2021-09-25-19-44-09_ma_gridworld_total_corr_slip_0p05.pkl'
 save_str = os.path.join(base_path, save_file_name)
 
 exp_logger = ExperimentLogger(load_file_str=save_str)
 
-save_file_name_reachability = '2021-09-24-16-44-35_ma_gridworld_reachability_slip_0p05.pkl'
+save_file_name_reachability = '2021-09-25-20-05-29_ma_gridworld_reachability_slip_0p05.pkl'
 save_str_reachability = os.path.join(base_path, save_file_name_reachability)
 exp_logger_reachability = ExperimentLogger(load_file_str=save_str_reachability)
 success_prob_reachability = exp_logger_reachability.results[0]['success_prob'] * np.ones((99,))
@@ -46,15 +46,14 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(iters, total_corr,
         color='blue', marker='.')
-ax.plot(iters, exp_logger_reachability.results[0]['total_corr'] * np.ones(len(iters)), color='magenta')
+# ax.plot(iters, exp_logger_reachability.results[0]['total_corr'] * np.ones(len(iters)), color='magenta')
 ax.grid()
 ax.set_ylabel('Total Correlation Value', fontsize=fontsize)
 ax.set_xlabel('Number of Convex-Concave Iterations', fontsize=fontsize)
 ax.set_title('Total Correlation During Policy Synthesis', fontsize=fontsize)
-ax.set_yscale('log')
 
 tikz_file_str = os.path.join(tikz_save_path, 'total_corr_vs_iters.tex')
-# tikzplotlib.save(tikz_file_str)
+tikzplotlib.save(tikz_file_str)
 
 plt.show()
 
@@ -62,18 +61,18 @@ plt.show()
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(iters, success_prob, 
-            color='green', marker='.', label='Success Probability [Full Comms]')
+            color='blue', marker='.', label='Success Probability [Full Comms]')
 ax.plot(iters, success_prob_reachability,
-            color='green', linestyle='--', 
+            color='red', marker='.', 
             label='Success Probability [Full Comms], \n Optimal Reachability Policy')
 ax.plot(iters, bound,
             color='black', 
             label='Theoretical Lower Bound on Success Probability of Imaginary Play')
 ax.plot(iters, empirical_imag,
-            color='magenta', marker='.', 
+            color='blue', linestyle='--', 
             label='Empirically Measured Success Probability of Imaginary Play')
 ax.plot(iters, empirical_imag_reachability,
-            color='magenta', linestyle='--',
+            color='red', linestyle='--',
             label='Empirically Measured Success Probability of Imaginary Play \n Optimal Reachability Policy')
 ax.grid()
 ax.set_ylabel('Total Correlation Value', fontsize=fontsize)
@@ -82,6 +81,6 @@ ax.set_title('Policy Success Probability During Synthesis', fontsize=fontsize)
 plt.legend(fontsize=fontsize)
 
 tikz_file_str = os.path.join(tikz_save_path, 'success_prob_vs_iters.tex')
-# tikzplotlib.save(tikz_file_str)
+tikzplotlib.save(tikz_file_str)
 
 plt.show()
