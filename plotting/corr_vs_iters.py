29ccc4776e10a73ef0ab6330c2c9fae04aa36788
Cyrus Neary <cyrusneary@gmail.com>
2021-10-08 18:00:23 -0500

    adding three-agent plotting code

--- plotting/corr_vs_iters.py
+++ plotting/corr_vs_iters.py
@@ -6,25 +6,27 @@ sys.path.append('..')
 
 import tikzplotlib
 
-from environments.ma_gridworld import MAGridworld
 from utils.experiment_logger import ExperimentLogger
 
 # Plotting parameters
 fontsize = 12
+num_data_points = 51
 
 tikz_save_path = os.path.abspath(os.path.join(os.path.curdir, 'tikz'))
 
 base_path = os.path.abspath(os.path.join(os.path.curdir, '..', 'examples', 'results'))
-save_file_name = '2021-09-25-19-44-09_ma_gridworld_total_corr_slip_0p05.pkl'
+# save_file_name = '2021-10-06-15-51-09_ma_gridworld_total_corr_slip_0p05.pkl' # data used in paper
+save_file_name = '2021-10-08-14-43-52_three_agent_gridworld_total_corr_0p05.pkl' # three agent example
 save_str = os.path.join(base_path, save_file_name)
 
 exp_logger = ExperimentLogger(load_file_str=save_str)
 
-save_file_name_reachability = '2021-09-25-20-05-29_ma_gridworld_reachability_slip_0p05.pkl'
+# save_file_name_reachability = '2021-10-06-16-09-44_ma_gridworld_reachability_0p05.pkl' # data used in paper
+save_file_name_reachability = '2021-10-08-14-40-21_three_agent_gridworld_reachability_0p05.pkl' # three agent example
 save_str_reachability = os.path.join(base_path, save_file_name_reachability)
 exp_logger_reachability = ExperimentLogger(load_file_str=save_str_reachability)
-success_prob_reachability = exp_logger_reachability.results[0]['success_prob'] * np.ones((99,))
-empirical_imag_reachability = exp_logger_reachability.results[0]['empirical_imag_success_rate'] * np.ones((99,))
+success_prob_reachability = exp_logger_reachability.results[0]['success_prob'] * np.ones((num_data_points,))
+empirical_imag_reachability = exp_logger_reachability.results[0]['empirical_imag_success_rate'] * np.ones((num_data_points,))
 
 # Get the relevant data in numpy format
 iters_indexes = exp_logger.results.keys()
@@ -33,7 +35,7 @@ total_corr = []
 success_prob = []
 empirical_imag = []
 iters = []
-for key in range(max(iters_indexes)):
+for key in range(num_data_points):
     total_corr.append(exp_logger.results[key]['total_corr'])
     success_prob.append(exp_logger.results[key]['success_prob'])
     empirical_imag.append(exp_logger.results[key]['empirical_imag_success_rate'])
@@ -52,7 +54,7 @@ ax.set_ylabel('Total Correlation Value', fontsize=fontsize)
 ax.set_xlabel('Number of Convex-Concave Iterations', fontsize=fontsize)
 ax.set_title('Total Correlation During Policy Synthesis', fontsize=fontsize)
 
-tikz_file_str = os.path.join(tikz_save_path, 'total_corr_vs_iters.tex')
+tikz_file_str = os.path.join(tikz_save_path, 'total_corr_vs_iters_three_agent.tex')
 tikzplotlib.save(tikz_file_str)
 
 plt.show()
@@ -80,7 +82,7 @@ ax.set_xlabel('Number of Convex-Concave Iterations', fontsize=fontsize)
 ax.set_title('Policy Success Probability During Synthesis', fontsize=fontsize)
 plt.legend(fontsize=fontsize)
 
-tikz_file_str = os.path.join(tikz_save_path, 'success_prob_vs_iters.tex')
+tikz_file_str = os.path.join(tikz_save_path, 'success_prob_vs_iters_three_agent.tex')
 tikzplotlib.save(tikz_file_str)
 
 plt.show()
